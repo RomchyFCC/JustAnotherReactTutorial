555eca260763cd44b56f1a03b27032927ee2b5db
Roman Struna <roman.struna.dev@gmail.com>
2018-09-22 23:18:05 +0200

    adding redux store and reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,19 @@
 const initState = {
   posts: [
-    {id: 1, title: 'title1', body: 'body1'},
-    {id: 2, title: 'title2', body: 'body2'},
-    {id: 3, title: 'title3', body: 'body3'},
+    {id: '1', title: 'title1', body: 'body1'},
+    {id: '2', title: 'title2', body: 'body2'},
+    {id: '3', title: 'title3', body: 'body3'},
   ]
 }
 
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'DELETE_POST') {
+    let newPosts = state.posts.filter(post => post.id !== action.id);
+    return {
+      ...state,
+      posts: newPosts
+    }
+  }
   return state;
 }
 
